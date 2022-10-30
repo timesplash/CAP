239772bba51ef367f7b2e1ec4ffa481ccce3e0e0
timesplash <timesplash@yandex.ru>
2022-10-30 22:04:09 +0300

    User controller - initial stuff Should be working now, not everything what I wanted is done, but it is ready to use

--- CAP-backend/src/main/java/org/dev/backend/controller/UserController.java
+++ CAP-backend/src/main/java/org/dev/backend/controller/UserController.java
@@ -1,4 +1,56 @@
 package org.dev.backend.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.dev.backend.model.User;
+import org.dev.backend.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@CrossOrigin
+@RequestMapping("/api/users")
 public class UserController {
+    private final UserService userService;
+
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PutMapping
+    void saveUser(User user) {
+        log.info("Saving new user...");
+        userService.saveUser(user);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<HttpStatus> deleteUser(@PathVariable("id") int id) {
+        try {
+            log.info("Deleting user. Great success!");
+            userService.deleteUser(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            log.info("Deleting user. WHAT?!?! YOU LIED TO ME! You lied about California!");
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(value = "/{login}")
+    public ResponseEntity<User> findByLogin(@PathVariable("login") String login) throws Exception {
+        log.info("Finding user by login = " + login);
+        return new ResponseEntity<>(userService.findByLogin(login), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        log.info("Finding all users");
+        return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
+    }
+
+
 }
+
