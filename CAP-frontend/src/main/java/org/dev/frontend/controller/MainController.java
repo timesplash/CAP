36a714bd0a877aa6ad254706dea9f6c1bc918d60
timesplash <timesplash@yandex.ru>
2022-11-10 23:17:17 +0300

    MainController and window

--- CAP-frontend/src/main/java/org/dev/frontend/controller/MainController.java
+++ CAP-frontend/src/main/java/org/dev/frontend/controller/MainController.java
@@ -1,4 +1,56 @@
 package org.dev.frontend.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import org.dev.api.enums.Role;
+import org.dev.frontend.store.StoreRestUtils;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.nio.file.AccessDeniedException;
+
 public class MainController {
+    private final StoreRestUtils storeRestUtils = StoreRestUtils.getInstance();
+
+    @FXML
+    private Button register;
+
+    @FXML
+    private Button logInBtn;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private VBox all;
+
+    @FXML
+    private void initialize(){
+        all.setStyle("-fx-background-color: #515151");
+        if(storeRestUtils.anyUsersPresent()) {
+
+        }
+        logInBtn.setDefaultButton(true);
+    }
+
+    @FXML
+    private void handleLogInBtnClicked(ActionEvent event) throws IOException {
+        try {
+            if(storeRestUtils.login(username.getText() , password.getText()) == Role.ADMIN){
+
+            } else if (storeRestUtils.login(username.getText() , password.getText()) == Role.CORPORATE) {
+
+            } else {
+
+            }
+        } catch (AccessDeniedException e) {
+
+        }
+    }
 }
--- CAP-frontend/src/main/java/org/dev/frontend/store/StoreRestUtils.java
+++ CAP-frontend/src/main/java/org/dev/frontend/store/StoreRestUtils.java
@@ -1,17 +1,36 @@
 package org.dev.frontend.store;
 
 import lombok.Getter;
+import lombok.Setter;
+import org.dev.api.enums.Role;
 import org.dev.api.model.UserDTO;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+
+import java.nio.file.AccessDeniedException;
 
 public class StoreRestUtils {
     private static final StoreRestUtils storeRestUtils = new StoreRestUtils();
 
     public static final String ANY_USERS_REQUEST_URL = "http://localhost:8888/api/users";
 
+    public static final String GET_USER_BY_LOGIN_REQUEST_URL = "http://localhost:8888/api/users/";
+
+    public static final String SAVE_USER_REQUEST_URL = "http://localhost:8888/api/users/";
+
     public static StoreRestUtils getInstance(){
         return storeRestUtils;
     }
+
+    @Setter
+    private String username;
+
+    @Setter
+    private String password;
 
     @Getter
     private UserDTO currentUser;
@@ -20,5 +39,24 @@ public class StoreRestUtils {
 
     public Boolean anyUsersPresent(){
         return restTemplate.getForObject(ANY_USERS_REQUEST_URL, Boolean.class);
+    }
+
+    public Role login(String username, String password) throws AccessDeniedException {
+        this.username = username;
+        this.password = password;
+        restTemplate.getInterceptors().clear();
+        restTemplate.getInterceptors().add(new BasicAuthenticationInterceptor(username, password));
+        try {
+            ResponseEntity<UserDTO> userDTOResponseEntity = restTemplate.exchange(GET_USER_BY_LOGIN_REQUEST_URL + this.username,
+                    HttpMethod.GET, new HttpEntity<>(restTemplate.getInterceptors()), UserDTO.class);
+            currentUser = userDTOResponseEntity.getBody();
+            return currentUser.getRole();
+        } catch (HttpClientErrorException e) {
+            throw new AccessDeniedException("");
+        }
+    }
+
+    public void saveUser(UserDTO userDTO) {
+        restTemplate.put(SAVE_USER_REQUEST_URL, userDTO);
     }
 }
